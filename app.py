59df467cb575721aee6feafc040f57d3803d214b
Ramesh Bhutka <60956788+Ramesh-Bhutka@users.noreply.github.com>
2021-06-08 17:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def car_price_prediction():
 
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
